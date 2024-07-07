c0c4fcdf11eed03b0b53009efeff133204472eb8
bandhandut19 <bondon21081@gmail.com>
2024-07-07 17:37:12 +0600

    localState login setup done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,34 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../redux/features/auth/verifyToken";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "0001",
+      password: "admin12345",
+    },
+  });
+
+  const [login] = useLoginMutation();
+  const dispatch = useAppDispatch();
+
+  type TOnsubmit = {
+    id: string;
+    password: string;
+  };
+
+  const onSubmit = async (data: TOnsubmit) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
